c46e4bf945c426a8b2c050814730dcee017f0a34
softsbuild <megaplusms2@gmail.com>
2026-08-28 09:19:55 -0300

    criação do projeto no Visual Studio/ Extensões baixadas

---
+++ appReversotask/appReversotask/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace appReversotask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
